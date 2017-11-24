4f529c19c6b460821fdf092a437a4a117e40e96f
Lingtong Sun <lingtongsun@gmail.com>
2017-11-24 12:29:44 -0800

    save

--- interview.py
+++ interview.py
@@ -2,6 +2,7 @@ from collections import deque
 arr = [1, 2, 3, 4, 5, 6, 7, None, 8, 9, 10]
 arr2 = [1, 2, 3, 4, 5, 6, None, 7, 8, None, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 arr3 = [1, None, None, 2, 3, 4]
+arr4 = [1, None, 2, 3, 4, 5, None, 6, 7, 8, None, 9, 10]
 
 class Node:
     def __init__(self, value, next_node, lc, rc, parent):
@@ -20,7 +21,7 @@ def print_tree(root):
     while thislevel:
         nextlevel = list()
         for n in thislevel:
-            print(n.value, " ", end="")
+            # print(n.value, " ", end="")
             if n.left_child: nextlevel.append(n.left_child)
             if n.right_child: nextlevel.append(n.right_child)
         print ("\n")
@@ -34,9 +35,6 @@ def parse(input_array):
         new_node = Node(item, None, None, None, None)
 
         print(item)
-        if curr is None:
-            raise Exception("Invalid input provided")
-        print("curr:" + str(curr))
 
         if root is None:
             root = new_node
@@ -44,25 +42,28 @@ def parse(input_array):
             last_placed = root
             continue
 
+        if curr is None:
+            raise Exception("Invalid input provided")
+        print("curr:" + str(curr))
+
         if curr.left_child is None:
             curr.left_child = new_node
             print(" left")
         elif curr.right_child is None:
             print(" right")
-
             curr.right_child = new_node
-
-            print(" currNext:" + str(curr.next_node))
-            curr = curr.next_node
 
         print("     last placed:" + str(last_placed))
         if new_node.value is not None:
             last_placed.next_node = new_node
             last_placed = new_node
 
+        if curr.right_child is not None:
+            print(" currNext:" + str(curr.next_node))
+            curr = curr.next_node
 
     return root
 
 
-root = parse(arr3)
+root = parse(arr4)
 print_tree(root=root)
