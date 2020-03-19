6d90e4a151d7d5ab974baa2b22a84a565b303f29
Lingtong Sun <lingtongsun@fb.com>
2020-03-19 14:19:47 -0700

    import deuces

--- cs234/poker_rl/hand_strength.py
+++ cs234/poker_rl/hand_strength.py
@@ -1,1 +1,15 @@
 import numpy as np 
+
+from deuces.deuces import Card
+
+board = [
+    Card.new('Ah'),
+    Card.new('Kd'),
+    Card.new('Jc')
+]
+hand = [
+   Card.new('Qs'),
+   Card.new('Th')
+]
+
+Card.print_pretty_cards(board + hand)
